c0704c44eba591ad9438e05bba23534c32dda469
Dennis Aleynikov <daleynikov@gmail.com>
2014-10-04 15:28:12 -0700

    Installed f7

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require framework7
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
