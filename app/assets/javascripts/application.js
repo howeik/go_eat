33ef8c94295a6d34e270f1eb5665582c801b2f36
Dennis Aleynikov <daleynikov@gmail.com>
2014-10-05 10:54:46 -0700

    Strip away the bullshit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require framework7
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
