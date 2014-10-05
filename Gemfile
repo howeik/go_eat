b8ded01f1a3a60fedf9ff0d18752dba17a07c547
Nicholas Sun <olassun2@gmail.com>
2014-10-04 21:35:22 -0700

    google places too

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'spring',        group: :development
 gem 'faraday'
 gem 'faraday_middleware'
 
+gem 'google_places'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,36 +1,60 @@
+require 'net/http'
+require 'uri'
+require 'opentable_client'
+
 class PagesController < ApplicationController
-	def main
-		unless params.key? 'lat'
-			get_location
-			return
-		end
+  def main
+    get_result
+  end
+=begin
+  def main
+    unless params.key? 'lat'
+      get_location
+      return
+    end
 
-		unless params.key? 'num_people'
-			get_num_people
-			return
-		end
+    unless params.key? 'num_people'
+      get_num_people
+      return
+    end
 
-		unless params.key? 'budget'
-			get_budget
-			return
-		end
+    unless params.key? 'budget'
+      get_budget
+      return
+    end
 
-		get_result
-	end
+    get_result
+  end
+=end
 
-	def get_location
-		render 'get_location'
-	end
+  def get_location
+    render 'get_location'
+  end
 
-	def get_num_people
-		render 'get_num_people'
-	end
+  def get_num_people
+    render 'get_num_people'
+  end
 
-	def get_budget
-		render 'get_budget'
-	end
+  def get_budget
+    render 'get_budget'
+  end
 
-	def get_result
-		render 'get_result'
-	end
+  def get_result
+=begin
+    api = OpenTable::Client.new
+    resp = api.restaurants(:city => "Chicago")
+
+    # Process response
+    puts 'hi we here'
+    puts resp['total_entries']       # => records found
+    puts 'hi we done'
+    resp['restaurants'] # => restaurant records
+=end
+    api = OpenTable::Client.new
+    resp = api.restaurants(:city => "Chicago")
+
+    puts resp
+
+    render 'get_result'
+  end
 end
